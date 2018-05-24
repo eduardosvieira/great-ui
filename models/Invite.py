9e1f3fbc5e80bd8aad3508fad79af6748e619cb3
Eduardo <euodeionomedeusuario@gmail.com>
2018-05-24 09:26:49 -0300

    implementing createinvite method

--- models/Invite.py
+++ models/Invite.py
@@ -8,3 +8,16 @@ class Invite():
         self.class = class
         self.createdAt = createdAt
         self.status = status
+
+    def createInvite(self, invite=None):
+        try:
+            db.invites.insert({
+                "user": invite.user,
+                "class": invite.class,
+                "createdAt": invite.createdAt,
+                "status": invite.status
+            })
+
+            return True
+        except:
+            return False
