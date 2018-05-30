d53e8a1d7262636ac08cc834a7806780dcf795e3
Eduardo <euodeionomedeusuario@gmail.com>
2018-05-30 15:41:13 -0300

    implementing route to update class

--- great/controllers/ClassController.py
+++ great/controllers/ClassController.py
@@ -40,3 +40,14 @@ def get_class(class_id):
     classe = Class().getClassById(class_id)
 
     return classe
+
+@app.route("/classroom/classes/<class_id>/", methods=["PUT"])
+def update_class(class_id):
+    name = request.form.get("name")
+    description = request.form.get("description")
+
+    classe = Class(name=name, description=description)
+
+    classe.updateClass(classe)
+
+    return "OK", 200
