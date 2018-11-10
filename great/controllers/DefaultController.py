42b073dc0a9cf6906a9cb7d1dfe2642716818cfc
eduardom <euodeionomedeusuario@gmail.com>
2018-11-10 18:31:15 -0300

    adding about.html

--- great/controllers/DefaultController.py
+++ great/controllers/DefaultController.py
@@ -7,6 +7,10 @@ from great.models.Class import Class
 def index():
     return render_template('index.html')
 
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
 @app.route('/dashboard/')
 def dashboard():
     return render_template('dashboard.html')
